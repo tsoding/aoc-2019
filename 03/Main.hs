dbd93976e3744e3217555867bbec0ff50836822f
rexim <reximkut@gmail.com>
2020-12-31 22:14:37 +0700

    [03] Add Day 3 Part 2 solution

--- 03/Main.hs
+++ 03/Main.hs
@@ -8,7 +8,11 @@ import Text.Printf
 import Data.Maybe
 import Data.Function
 
+type Steps = Int
 type Cell = (Int, Int)
+
+(^-^) :: Cell -> Cell -> Cell
+(^-^) (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
 
 data Seg
   = Hor Int
@@ -17,12 +21,23 @@ data Seg
         (Int, Int)
   deriving (Show)
 
-inter :: Seg -> Seg -> Maybe Cell
-inter (Hor y (x1, x2)) (Ver x (y1, y2))
+type Vec = (Cell, Steps, Dir)
+
+vec2Seg :: Vec -> Seg
+vec2Seg (begin, _, dir) = mkSeg begin $ moveTo begin dir
+
+interVecs :: Vec -> Vec -> Maybe (Cell, Steps)
+interVecs vec1@(begin1, steps1, _) vec2@(begin2, steps2, _) = do
+  end <- interSegs (vec2Seg vec1) (vec2Seg vec2)
+  let steps = steps1 + steps2 + dist (end ^-^ begin1) + dist (end ^-^ begin2)
+  return (end, steps)
+
+interSegs :: Seg -> Seg -> Maybe Cell
+interSegs (Hor y (x1, x2)) (Ver x (y1, y2))
   | x1 <= x && x <= x2 && y1 <= y && y <= y2 = Just (x, y)
   | otherwise = Nothing
-inter ver@(Ver _ _) hor@(Hor _ _) = inter hor ver
-inter _ _ = Nothing
+interSegs ver@(Ver _ _) hor@(Hor _ _) = interSegs hor ver
+interSegs _ _ = Nothing
 
 data Dir
   = R Int
@@ -53,6 +68,14 @@ segsOfWire dirs = zipWith mkSeg ps (tail ps)
   where
     ps = scanl moveTo (0, 0) dirs
 
+vecsOfWire :: [Dir] -> [Vec]
+vecsOfWire dirs = zip3 ps ss dirs
+  where
+    ps = scanl moveTo (0, 0) dirs
+    ss =
+      scanl (\steps (begin, end) -> dist (end ^-^ begin) + steps) 0 $
+      zip ps (tail ps)
+
 parseDir :: T.Text -> Dir
 parseDir input =
   case T.uncons input of
@@ -62,10 +85,10 @@ parseDir input =
     Just ('D', s) -> D $ read $ T.unpack s
     _ -> error $ printf "`%s` is not a correct direction" input
 
-parseWire :: T.Text -> [Seg]
-parseWire = segsOfWire . map parseDir . T.splitOn ","
+parseWire :: T.Text -> [Dir]
+parseWire = map parseDir . T.splitOn ","
 
-readInput :: FilePath -> IO ([Seg], [Seg])
+readInput :: FilePath -> IO ([Dir], [Dir])
 readInput filePath = do
   first:second:_ <- T.lines <$> T.readFile filePath
   return $ (parseWire first, parseWire second)
@@ -73,11 +96,23 @@ readInput filePath = do
 dist :: (Int, Int) -> Int
 dist (x, y) = abs x + abs y
 
-inters :: ([Seg], [Seg]) -> [(Int, Int)]
-inters (xs, ys) = catMaybes [inter x y | x <- xs, y <- ys]
+allSegsInters :: ([Seg], [Seg]) -> [(Int, Int)]
+allSegsInters (xs, ys) = catMaybes [interSegs x y | x <- xs, y <- ys]
 
-part1 :: ([Seg], [Seg]) -> Int
-part1 (xs, ys) = head $ filter (> 0) $ sort $ map dist $ inters (xs, ys)
+part1 :: ([Dir], [Dir]) -> Int
+part1 (xs, ys) =
+  head $
+  filter (> 0) $ sort $ map dist $ allSegsInters (segsOfWire xs, segsOfWire ys)
+
+allVecsInters :: ([Vec], [Vec]) -> [Steps]
+allVecsInters (xs, ys) = map snd $ catMaybes [interVecs x y | x <- xs, y <- ys]
+
+part2 :: ([Dir], [Dir]) -> Steps
+part2 (xs, ys) =
+  head $ filter (> 0) $ sort $ allVecsInters (vecsOfWire xs, vecsOfWire ys)
 
 main :: IO ()
-main = undefined
+main = do
+  input <- readInput "input.txt"
+  printf "Part 1: %d\n" $ part1 input
+  printf "Part 2: %d\n" $ part2 input
---
+++ 03/README.md
@@ -0,0 +1,8 @@
+# [Day 3](https://adventofcode.com/2019/day/3) solution
+
+## Expected Result
+
+```console
+Part 1: 1064
+Part 2: 25676
+```
